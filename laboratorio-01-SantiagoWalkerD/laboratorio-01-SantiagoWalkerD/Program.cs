d75a3c23cbf735f1a0d6c09010598a4cbe985cef
unknown <swalker@miuandes.cl>
2020-03-17 15:21:24 -0300

    first commit

---
+++ laboratorio-01-SantiagoWalkerD/laboratorio-01-SantiagoWalkerD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laboratorio_01_SantiagoWalkerD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
