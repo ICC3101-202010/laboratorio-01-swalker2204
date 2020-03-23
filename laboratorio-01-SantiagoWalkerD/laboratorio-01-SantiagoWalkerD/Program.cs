5f66ed1bab08b2ad0b619aacea96f41bc94b0fa9
unknown <swalker@miuandes.cl>
2020-03-22 21:53:22 -0300

    third commit

--- laboratorio-01-SantiagoWalkerD/laboratorio-01-SantiagoWalkerD/Program.cs
+++ laboratorio-01-SantiagoWalkerD/laboratorio-01-SantiagoWalkerD/Program.cs
@@ -7,6 +7,7 @@ namespace laboratorio_01_SantiagoWalkerD
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hola");
         }
     }
 }
